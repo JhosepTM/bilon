a9eb6ee455f288cc030c1964a375092b990b42f8
JoDemVel <josuedemetriovelizescobar@gmail.com>
2023-06-07 14:23:48 -0400

    solved problems

--- app.js
+++ app.js
@@ -9,15 +9,7 @@ import { routerUp } from "./app/routes/uploads.js";
 
 const port = process.env.PORT; 
 
-app.get('/api/xd', async (req, res) => {
-    try{
-        const {rows} = await pool.query('SELECT * FROM User'); 
-        res.json(rows);
-    }catch(e){
-        console.log("Error al obtener usuarios", e);
-        res.status(500).json({error: "Error al obtener usuarios"});
-    }
-});
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -26,17 +18,4 @@ app.use('/api', routerUp);
  
 app.listen(port, () => {
     console.log(`Servidor corriendo en el puerto ${port}`);
-});
-
-const express = require('express');
-const app = express();
-const routes = require('./app/routes/appRoutes');
-
-app.use(express.json());
-// Asociar las rutas configuradas en routes.js
-app.use('/api', routes);
-
-// Iniciar el servidor
-app.listen(port, () => {
-  console.log(`Servidor corriendo en el puerto ${port}`);
 });
